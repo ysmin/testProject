15123d8f22f0ca882c6f4aa93f7fdbf422c57f33
ysmin <yusng00@naver.com>
2015-01-02 10:56:20 +0900

    파일 올림

---
+++ index.php
@@ -0,0 +1,12 @@
+<?
+$dir = $_SERVER["DOCUMENT_ROOT"];
+include $dir."/inc/header/mainHeader.php";
+?>
+	
+	<span class="mainImgTop">돌보미는 사랑입니다</span>
+	<span class="mainImgBottom"></span>
+
+
+<?
+include $dir."/inc/footer/mainFooter.php";
+?>
